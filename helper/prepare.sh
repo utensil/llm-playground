cec292566f26aafe73993e338525ffaaa34c44e3
Utensil <utensilcandel@gmail.com>
2023-04-23 06:27:30 +0000

    Update prepare scripts

--- helper/prepare.sh
+++ helper/prepare.sh
@@ -2,8 +2,6 @@
 
 cd "$(dirname "$0")"  # change directory to where the script is located
 
-pip3 install -r requirements.txt
-
 SUDO=${SUDO:-"sudo"}
 
 if [ "$SUDO" = "nosudo" ]; then
@@ -14,3 +12,6 @@ $SUDO apt-get update
 
 cat packages.txt | xargs $SUDO apt-get install -y
 
+git lfs install
+
+pip3 install -r requirements.txt
