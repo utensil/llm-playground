e1db9f949cdffaad277f73b1c4c5a4c017eb3b90
Utensil <utensilcandel@gmail.com>
2023-05-29 08:05:57 +0000

    Add entry for axolotl

---
+++ scripts/entry/prepare_ax.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Container source: https://github.com/OpenAccess-AI-Collective/axolotl/blob/main/docker/Dockerfile-runpod
+#
+#
+# To run this in RunPod with `winglian/axolotl-runpod:main-cu118-2.0.0`, set
+# Expose HTTP Ports (Max 10): 7860,8888
+# docker command: `bash -c "curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/utensil/llm-playground/main/scripts/entry/prepare_ax.sh -sSf | bash"`
+# JUPYTER_PASSWORD change to your secret
+# HUGGINGFACE_TOKEN change to your token from https://huggingface.co/settings/tokens
+# SUDO nosudo
+# WORKSPACE /workspace/
+#
+# To test this in Codespaces, run `cd /workspaces/ && WORKSPACE=/workspaces/ llm-playground/scripts/entry/prepare_ax.sh`
+
+set -euxo pipefail
+
+set -x
+
+CODESPACES=${CODESPACES:-""}
+WORKSPACE=${WORKSPACE:-"/workspace"}
+
+cd $WORKSPACE
+
+if [ ! -d "llm-playground" ]; then
+  git clone https://github.com/utensil/llm-playground
+fi
+
+cd llm-playground
+
+export DEBIAN_FRONTEND=noninteractive
+./helper/prepare.sh
+
+cd $WORKSPACE/llm-playground
+
+LOAD_MODEL=${LOAD_MODEL:-""}
+LOAD_DATASET=${LOAD_DATASET:-""}
+
+if [ ! -z "$LOAD_MODEL" ]; then
+    python ./helper/download-model.py $LOAD_MODEL
+fi
+
+if [ ! -z "$LOAD_DATASET" ]; then
+    python ./helper/download-dataset.py $LOAD_DATASET
+fi
+
+mkdir -p $WORKSPACE/llm-playground/models
+mkdir -p $WORKSPACE/llm-playground/loras
+mkdir -p $WORKSPACE/llm-playground/datasets
+
+python ./helper/storage.py
+
+bash $WORKSPACE/llm-playground/scripts/prepare_jupyter.sh
+bash $WORKSPACE/llm-playground/scripts/prepare_qlora.sh
+
+JUPYTER_PASSWORD=${JUPYTER_PASSWORD:-"pytorch"}
+
+bash $WORKSPACE/llm-playground/scripts/nohup_jupyter.sh
+
+sleep infinity
--- scripts/run_jupyter.sh
+++ scripts/run_jupyter.sh
@@ -1,1 +1,6 @@
+WORKSPACE=${WORKSPACE:-"/workspace"}
+JUPYTER_PASSWORD=${JUPYTER_PASSWORD:-"pytorch"}
+
+echo "Launching Jupyter Lab..."
+cd /
 jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=$WORKSPACE
