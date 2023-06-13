c29757b378bebc450a01717a9c817670c52d8c8b
Utensil <utensilcandel@gmail.com>
2023-06-13 14:48:14 +0800

    Support optionally specify local storage root

--- helper/storage.py
+++ helper/storage.py
@@ -2,8 +2,12 @@ from huggingface_hub import Repository, repository
 import argparse
 import os
 
+DEFAULT_REPO = 'utensil/storage'
+DEFAULT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+
 parser = argparse.ArgumentParser()
-parser.add_argument('REPO', type=str, default='utensil/storage', nargs='?')
+parser.add_argument('REPO', type=str, default=DEFAULT_REPO, nargs='?', help="The HF repo to upload to: <user_name>/<repo_name>")
+parser.add_argument('ROOT', type=str, default=DEFAULT_ROOT, nargs='?', help="The local repo will clone to ROOT/<repo_name>")
 parser.add_argument('-m', '--model', type=bool, nargs='?', default=False, const=True, help='Set repo type to model (default: dataset)')
 parser.add_argument('-p', '--pull', type=bool, nargs='?', default=False, const=True, help='Whether to pull before push')
 parser.add_argument('-u', '--upload', type=bool, nargs='?', default=False, const=True, help='Whether to upload local changes')
@@ -12,13 +16,11 @@ parser.add_argument('-l', '--lfs', type=bool, nargs='?', default=False, const=Tr
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    # determine the root of the repo and cd to it
-    ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
-    os.chdir(ROOT)
-    print(f"Working directory changed to: {ROOT}")
-
     HF_TOKEN = os.environ.get("HUGGINGFACE_TOKEN")
-
+    
+    ROOT = args.ROOT
+    os.chdir(ROOT)
+    
     remote_repo = args.REPO
     local_repo = remote_repo.split('/')[-1]
 
