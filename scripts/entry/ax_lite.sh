fc55a4ae3ca1ec73b2b7d3c60b32c3deb09763f8
Utensil <utensilcandel@gmail.com>
2023-06-15 22:08:32 +0800

    Setup cache and avoid reclone

--- scripts/entry/ax_lite.sh
+++ scripts/entry/ax_lite.sh
@@ -11,6 +11,8 @@
 # WORKSPACE /workspace/
 # WANDB_API_KEY change to your key from https://wandb.ai/authorize
 #
+# MUST mount volume disk at /content/
+#
 # To test this in Codespaces, run `cd /workspaces/ && WORKSPACE=/workspaces/ llm-playground/scripts/entry/ax_lite.sh`
 
 set -euxo pipefail
@@ -21,6 +23,11 @@ WORKSPACE=${WORKSPACE:-"/workspace"}
 
 export DEBIAN_FRONTEND=noninteractive
 
+# make the cache live on volume disk
+rm -rf /root/.cache
+mkdir -p /content/cache
+ln -s /content/cache /root/.cache
+
 # prepare jupyter
 pip install jupyterhub notebook jupyterlab jupyterlab-git ipywidgets
 
@@ -29,14 +36,25 @@ pip install nvitop
 
 # update axolotl
 cd $WORKSPACE
-git clone https://github.com/OpenAccess-AI-Collective/axolotl axolotl-update
+if [ ! -d "axolotl-update" ]; then
+  git clone https://github.com/OpenAccess-AI-Collective/axolotl axolotl-update
+# don't update between run yet
+# else
+#   cd axolotl-update && git pull && cd ..
+fi
 cp -r axolotl-update/* axolotl
 cd axolotl
 # but don't install yet
 # pip install -e .
 
 mkdir -p /content/
-cd /content/ && git clone https://github.com/utensil/llm-playground
+cd /content/
+if [ ! -d "llm-playground" ]; then
+  git clone https://github.com/utensil/llm-playground
+# don't update between run yet
+# else
+#   cd llm-playground && git pull && cd ..
+fi
 
 cd $WORKSPACE
 
